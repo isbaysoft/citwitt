d247abeee72cb23dfca92e194df583c49aa2e7fe
isbaysoft <isbaysoft@gmail.com>
2013-04-29 02:04:03 +0300

    changed db

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = 'uOO5ytsgcKFDhyn46Wrw'
+  config.consumer_secret = 'hUfpwiu3xkJBM40NrgveNC1MukkMIYH0o3kmpWlaas'
+  config.oauth_token = '1321011650-5klzNMLb6Ef9hJEo1ueXe2y89bnlmVKk04BdLD2'
+  config.oauth_token_secret = 'prhxXpNMvCkBMdfhX5ULii0fgdTu54Vq1hOEwdk'
+end
