fd7bd6d2421519f278116a216b5827c0376cac56
isbaysoft <isbaysoft@gmail.com>
2013-04-29 01:59:24 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Citwitt::Application.config.secret_token = 'bbc4b55f920cc3166525523c98a44d100939b76350f681f896409bd9bc2c2cb1400435e7278b517f2ffabf9112a78e8f4702694d1b56310054edbbe6bb9a7908'
